bf5d6490910df447bb1dab8ba949d3cbe1067f2e
hs238307 <70473581+hs238307@users.noreply.github.com>
2024-04-11 17:19:35 +0530

    git

---
+++ backend/Assignment1/1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class MainClass
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                Console.Write("Type: 1 - Exercise1, 2 - Exercise2, 3 - Exercise3  =>  ");
+                string s = Console.ReadLine();
+                switch (s)
+                {
+                    case "1":
+                        Program1 firstQuestion = new Program1();
+                        firstQuestion.Question();
+                        break;
+                    case "2":
+                        Program2 secondQuestion = new Program2();
+                        secondQuestion.Question();
+                        break;
+                    case "3":
+                        Program3 thirdQuestion = new Program3();
+                        thirdQuestion.Question();
+                        break;
+                    default:
+                        Console.WriteLine("Wrong input");
+                        break;
+                }
+                Console.Write("Enter c to continue or any key to exit");
+                string str = Console.ReadLine();
+                if (!str.Equals("c")) break;
+            }
+        }
+    }
+    public class Program1
+    {
+        public void Question()
+        {
+            Program1 Pro1 = new Program1();
+            Console.Write("Enter Input :- ");
+            string s = Console.ReadLine();
+            Console.Write("Choose 1 : int, 2 : float, 3 : boolen :- ");
+            string c = Console.ReadLine();
+            switch (c)
+            {
+                case "1":
+                    Pro1.convertInt(s);
+                    break;
+                case "2":
+                    Pro1.convertFloat(s);
+                    break;
+                case "3":
+                    Pro1.convertBoolen(s);
+                    break;
+                default:
+                    Console.WriteLine("Wrong input");
+                    break;
+            }
+        }
+        public void convertInt(string s)
+        { 
+            try
+            {
+                // Using Int32.Parse
+                Console.Write("Using int32.Parse :- ");
+                Console.WriteLine(Int32.Parse(s));
+                // Using Convert.ToInt32
+                Console.Write("Using Convert.ToInt32 :- ");
+                Console.WriteLine(Convert.ToInt32(s));
+
+                // Using int.TryParse
+                int convertedInt;
+                Console.Write("Using int.TryParse :- ");
+                int.TryParse(s, out convertedInt);
+                Console.WriteLine(convertedInt);
+            }
+            catch{
+                Console.WriteLine("Error cause while converting to int bcz input is not in Integer formate");  
+            }
+        }
+        public void convertFloat(string s)
+        {
+            try
+            {
+                // Using float.Parse
+                Console.Write("Using float.Parse :- ");
+                Console.WriteLine(float.Parse(s));
+
+                // Using Convert.ToDouble
+                Console.Write("Using Convert.ToDouble :- ");
+                Console.WriteLine(Convert.ToDouble(s));
+
+                // Using flaot.TryParse
+                float convertedFloat;
+                Console.Write("Using float.TryParse:- ");
+                float.TryParse(s, out convertedFloat);
+                Console.WriteLine(convertedFloat);
+            }
+            catch
+            {
+                Console.WriteLine("Error cause while converting to int bcz input is not in Float formate");
+            }
+            
+        }
+        public void convertBoolen(string s)
+        {
+            try
+            {
+                // Using bool.Parse
+                Console.Write("Using bool.Parse :- ");
+                Console.WriteLine(bool.Parse(s));
+
+                // Using Convert.ToBoolean
+                Console.Write("Using Convert.ToBoolen :- ");
+                Console.WriteLine(Convert.ToBoolean(s));
+
+                // Using bool.TryParse
+                bool convertedBool;
+                Console.Write("Using bool.TryParse:- ");
+                bool.TryParse(s, out convertedBool);
+                Console.WriteLine(convertedBool);
+            }
+            catch
+            {
+                Console.WriteLine("Error cause while converting to int bcz input is not in Boolen formate");
+            }
+            
+        }
+    }
+
+
+
+
+
+
+    public class Program2
+    {
+
+        public void Question()
+        {
+            object firstObject = null; // Null Object
+            object secondObject = new object(); // A Non-null Object
+            object thirdObject = null; // Null Object
+            object fourthObject = new object(); // A Non-null Object
+
+            Console.WriteLine("For \"==\" Operator====> ");
+            Console.WriteLine("For {0} =>>> {1}", "(firstObject(Null) == secondObject(Non-null))", firstObject == secondObject);
+            Console.WriteLine("For {0} =>>> {1}", "(firstObject(Null) == thirdObject(Null))", firstObject == thirdObject);
+            Console.WriteLine("For {0} =>>> {1}", "(secondObject(Non-null) == thirdObject(Null))", secondObject == thirdObject);
+
+            Console.WriteLine("\nFor \"Equals\" Method====> ");
+            try 
+            { 
+                Console.WriteLine("For {0} =>>> {1}", "(secondObject(Non-null).Equals(fourthObject(Non-null)))",secondObject.Equals(fourthObject)); // Comparing Two Non-null objects
+            } 
+            catch (Exception e) 
+            { 
+                Console.WriteLine("For {0} =>>> {1}", "(secondObject(Non-null).Equals(fourthObject(Non-null)))", e.GetType()); 
+            }
+            try 
+            { 
+                Console.WriteLine("For {0} =>>> {1}", "(thirdObject(Null).Equals(firstObject(Null)))", thirdObject.Equals(firstObject)); // Comparing Two null object -- will give error bcz both are null and comparing a null object
+            } 
+            catch(Exception e) 
+            { 
+                Console.WriteLine("For {0} =>>> {1}", "(thirdObject(Null).Equals(firstObject(Null)))", e.GetType()); 
+            }
+            try 
+            { 
+                Console.WriteLine("For {0} =>>> {1}", "(firstObject(Null).Equals(secondObject(Non-null)))", firstObject.Equals(secondObject)); // Comparing null object to Non-null object will give error because we are comparing a null object 
+            } 
+            catch (Exception e) 
+            { 
+                Console.WriteLine("For {0} =>>> {1}", "(firstObject(Null).Equals(secondObject(Non-null)))", e.GetType()); 
+            }
+            try 
+            { 
+                Console.WriteLine("For {0} =>>> {1}", "(firstObject(Null).Equals(thirdObject(Null)))", firstObject.Equals(thirdObject)); // Comparing Two null object -- will give error bcz both are null and comparing a null object
+            } 
+            catch (Exception e) 
+            {
+                Console.WriteLine("For {0} =>>> {1}", "(firstObject(Null).Equals(thirdObject(Null)))", e.GetType()); 
+            }
+            try 
+            { 
+                Console.WriteLine("For {0} =>>> {1}", "(secondObject(Non-null).Equals(thirdObject(Null)))", secondObject.Equals(thirdObject)); // Comparing Non-null object with null give FALSE because we are comparing a non null object with null object. 
+            } 
+            catch (Exception e) 
+            { 
+                Console.WriteLine("For {0} =>>> {1}", "(secondObject(Non-null).Equals(thirdObject(Null)))", e.GetType()); 
+            }
+
+
+            Console.WriteLine("\nFor \"ReferenceEquals\" Method====> ");
+            Console.WriteLine("For {0} =>>> {1}", "ReferenceEquals(firstObject(Null),secondObject(Non-null)", ReferenceEquals(firstObject,secondObject));
+            Console.WriteLine("For {0} =>>> {1}", "ReferenceEquals(firstObject(Null),thirdObject(Null)", ReferenceEquals(firstObject,thirdObject));
+            Console.WriteLine("For {0} =>>> {1}", "ReferenceEquals(thirdObject(Null),firstObject(Null)", ReferenceEquals(thirdObject, firstObject));
+            Console.WriteLine("For {0} =>>> {1}", "ReferenceEquals(secondObject(Non-null),thirdObject(Null)", ReferenceEquals(secondObject,thirdObject));
+            Console.WriteLine("For {0} =>>> {1}", "ReferenceEquals(secondObject(Non-null),fourthObject(Non-null)", ReferenceEquals(secondObject, fourthObject));
+        }
+    }
+
+
+
+    public class Program3
+    {
+        public void Question()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter two numbers first number should be less than second number");
+                Console.Write("Enter first number -> ");
+                int firstNumber = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter second number -> ");
+                int secondNumber = Convert.ToInt32(Console.ReadLine());
+                if(firstNumber >= secondNumber)
+                {
+                    Console.WriteLine("First number should be greater then second number");
+                    Console.WriteLine("Please Enter Right Crediantial");
+                    continue;
+                }
+                else if(firstNumber<0 || secondNumber < 0)
+                {
+                    Console.WriteLine("Any or both of the number is negetive");
+                    Console.WriteLine("Please Enter Right Crediantial");
+                    continue;
+                }
+                else if(firstNumber<2 || firstNumber>1000 || secondNumber<2 || secondNumber>1000){
+                    Console.WriteLine("Any or both of the number is not is not in range (2 - 1000)");
+                    Console.WriteLine("Please Enter Right Crediantial");
+                    continue;
+                }
+
+                Program3 checkPrimeNumber = new Program3();
+                for(int i = firstNumber; i <= secondNumber; i++)
+                {
+                    if (checkPrimeNumber.checkPrime(i))
+                    {
+                        Console.Write("{0}, ",i);
+                    }
+                }
+                Console.WriteLine();
+                Console.Write("Enter c to continue or any key to exit");
+                string s = Console.ReadLine();
+                if (!s.Equals("c")) break;
+            }
+        }
+
+        public bool checkPrime(int number)
+        {
+            for(int i = 2; i <= number / 2; i++) // Run loop till half of the number because the number cann't devisible by second half number. So we check for second half numbers.  
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
+        }
+    }
+
+
+
+}
---
+++ backend/Assignment2/2.cs
@@ -0,0 +1,358 @@
+﻿using System;
+
+namespace Assignemt2
+{
+    public class mainClass
+    {
+        public static void Main()
+        {
+            Console.Write("Enter 4 for 1st question and 5 for 2nd question :- ");
+            string s = Console.ReadLine();
+            if (s.Equals("4"))
+            {
+                Program4 Pro = new Program4();
+                Pro.Question();
+            }
+            else if(s.Equals("5"))
+            {
+                Program5 pro = new Program5();
+                pro.Question();
+            }
+            else
+            {
+                Console.WriteLine("Enter right credentials ");
+            }
+        }
+    }
+    // Program for questiuon 4
+    public class Program4
+    {
+        public void Question()
+        {
+            while (true)
+            {
+                Equipment Equip;
+                Equip = new Functions();
+                Equip.setCredential();
+                Equip.setSpacificationOfEquipment();
+                while (true)
+                {
+                    Console.Write("Type distance :- ");
+                    string s = Console.ReadLine();
+                    int distance = Convert.ToInt32(s);
+                    Equip.movedBy(distance);
+                    Equip.maintenanaceCostIncrease(distance);
+                    Console.Write("Type r for result or anything for continue :- ");
+                    string str = Console.ReadLine();
+                    if (str.Equals("r"))
+                    {
+                        Equip.getEquipmentDetails();
+                        Console.Write("Type x for new Equipment or exit or anything for continue :- ");
+                        string sr = Console.ReadLine();
+                        if (sr.Equals("x")) break;
+                    }
+                }
+                Console.Write("Write x to exit or anything for continue");
+                string st = Console.ReadLine();
+                if (st.Equals("x")) break;
+            }
+        }
+    }
+
+    public class Functions:Equipment
+    {
+        public override void setCredential() // Function for set credential
+        {
+            Console.Write("Enter the name of equipment :- ");
+            name = Console.ReadLine();
+            Console.Write("Enter the description of equipment :- ");
+            description = Console.ReadLine();
+            Console.Write("Enter the type(0 -> mobile, 1-> imMobile) of equipment :- ");
+            string s = Console.ReadLine();
+            int position = Convert.ToInt32(s);
+            try
+            {
+                type = Enum.GetName(typeof(typeOfEquipment), position);
+            }
+            catch
+            {
+                Console.WriteLine("Please select the correct equipment type");
+            }
+            
+            base.setCredential();
+        }
+
+
+        public override void setSpacificationOfEquipment()
+        {
+            try
+            {
+                if (type.Equals("mobile"))
+                {
+                    Console.Write("Enter number of wheels in equipment :- ");
+                    string numberOfWheels = Console.ReadLine();
+                    
+                    try
+                    {
+                        spacificationOfEqipment = Convert.ToInt32(numberOfWheels);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Wheels should be a number!");
+                    }
+                }
+                else if (type.Equals("imMobile"))
+                {
+                    Console.Write("Enter weight of equipment :- ");
+                    string weightOfEquipment = Console.ReadLine();
+                    try
+                    {
+                        spacificationOfEqipment = Convert.ToInt32(weightOfEquipment);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Weight should be a number!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter right type of equipment!");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Equipment type is not derfined!");
+            }
+        }
+
+        public override void movedBy(int distance)
+        {
+            distanceMovedTillDate += distance;
+        }
+        public override void maintenanaceCostIncrease(int distance)
+        {
+            maintenanceCost += spacificationOfEqipment * distance;
+        }
+
+        public override void getEquipmentDetails()
+        {
+            Console.WriteLine("{0} :- {1}", "Name of the equipment :- ", name);
+            Console.WriteLine("{0} :- {1}", "Type of the equipment :- ", type);
+            Console.WriteLine("{0} :- {1}", "Description of the equipment :- ", description);
+            Console.WriteLine("{0} :- {1}", "Distance moved till date :- ", distanceMovedTillDate);
+            Console.WriteLine("{0} :- {1}", "Total Maintenance cost :- ", maintenanceCost);
+            if (type.Equals("mobile"))
+            {
+                Console.WriteLine("{0} :- {1}", "Wheels in eqipment :- ", spacificationOfEqipment);
+            }
+            else
+            {
+                Console.WriteLine("{0} :- {1}", "Weight of eqipment :- ", spacificationOfEqipment);
+            }
+        }
+    }
+
+    public abstract class Equipment // Main Equipment Abstract Class 
+    {
+        public string name;
+        public string description;
+        public int distanceMovedTillDate;
+        public int maintenanceCost;
+        public enum typeOfEquipment { mobile, imMobile };
+        public string type;
+        public int spacificationOfEqipment;
+        public virtual void setCredential()
+        {
+            distanceMovedTillDate = 0;
+            maintenanceCost = 0;
+        }
+        public abstract void setSpacificationOfEquipment();
+        public abstract void movedBy(int distance);
+        public abstract void maintenanaceCostIncrease(int distance);
+        public abstract void getEquipmentDetails();
+    }
+
+
+
+    // Program For Question 5
+
+    public class Program5
+    {
+        public void Question()
+        {
+            while (true)
+            {
+                Duck duck = new Duck();
+                Console.Write("Enter the type of duck 0 - Rubber, 1 - Mallard, 2 - Redhead :- ");
+                int duckNumber = Convert.ToInt32(Console.ReadLine());
+                switch (duckNumber)
+                {
+                    case 0:
+                        Rubber rubber = new Rubber();
+                        Console.Write("Enter the weight of duck :- ");
+                        int rubberWeight = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter the Number of Wings in the duck :- ");
+                        int rubberNumberOfWings = Convert.ToInt32(Console.ReadLine());
+                        rubber.setWeight(rubberWeight);
+                        rubber.setNumberOfWings(rubberNumberOfWings);
+                        rubber.setTypeOfDuck(duckNumber);
+                        rubber.setFlyStatus();
+                        rubber.setQueakStatus();
+                        rubber.showResult();
+                        break;
+                    case 1:
+                        Mallard mallard = new Mallard();
+                        Console.Write("Enter the weight of duck :- ");
+                        int mallardWeight = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter the Number of Wings in the duck :- ");
+                        int mallardNumberOfWings = Convert.ToInt32(Console.ReadLine());
+                        mallard.setWeight(mallardWeight);
+                        mallard.setNumberOfWings(mallardNumberOfWings);
+                        mallard.setTypeOfDuck(duckNumber);
+                        mallard.setFlyStatus();
+                        mallard.setQueakStatus();
+                        mallard.showResult();
+                        break;
+                    case 2:
+                        Redhead redhead = new Redhead();
+                        Console.Write("Enter the weight of duck :- ");
+                        int redheadWeight = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter the Number of Wings in the duck :- ");
+                        int redheadNumberOfWings = Convert.ToInt32(Console.ReadLine());
+                        redhead.setWeight(redheadWeight);
+                        redhead.setNumberOfWings(redheadNumberOfWings);
+                        redhead.setTypeOfDuck(duckNumber);
+                        redhead.setFlyStatus();
+                        redhead.setQueakStatus();
+                        redhead.showResult();
+                        break;
+                    default:
+                        Console.WriteLine("Wrong input");
+                        break;
+                }
+                
+                Console.Write("Type x to exit or type anything to continue :- ");
+                string s = Console.ReadLine();
+                if (s.Equals("x")) break;
+            }
+        }
+    }
+
+    public interface IDuckProperties{
+        void setFlyStatus();
+        void setQueakStatus();
+    }
+
+    public class Duck
+    {
+        public int weight;
+        public int numberOfWings;
+        public string typeOfDuck;
+        public string flyStatus;
+        public string quackStatus;
+        public enum typeOfDucks {
+            Rubber,
+            Mallard,
+            Redhead
+        }
+        public void showResult()
+        {
+            Console.WriteLine("{0} :- {1}", "The type of duck", typeOfDuck);
+            Console.WriteLine("{0} :- {1}", "The weight of duck", weight);
+            Console.WriteLine("{0} :- {1}", "The number of wings", numberOfWings);
+            Console.WriteLine("{0} :- {1}", "The Flying status of duck", flyStatus);
+            Console.WriteLine("{0} :- {1}", "The Quack status of duck", quackStatus);
+        }
+    }
+    public class Rubber:Duck, IDuckProperties
+    {
+        public void setWeight(int weight)
+        {
+            this.weight = weight;
+        }
+        public void setNumberOfWings(int numberOfWings)
+        {
+            this.numberOfWings = numberOfWings;
+        }
+        public void setTypeOfDuck(int duckNumber)
+        {
+            try
+            {
+                typeOfDuck = Enum.GetName(typeof(typeOfDucks), duckNumber);
+            }
+            catch
+            {
+                Console.WriteLine("Wrong duck number");
+            }
+        }
+        public void setFlyStatus()
+        {
+            flyStatus = "Don't fly";
+        }
+        public void setQueakStatus()
+        {
+            quackStatus = "Squack";
+        }
+    }
+    public class Mallard :Duck, IDuckProperties
+    {
+        public void setWeight(int weight)
+        {
+            this.weight = weight;
+        }
+        public void setNumberOfWings(int numberOfWings)
+        {
+            this.numberOfWings = numberOfWings;
+        }
+        public void setTypeOfDuck(int duckNumber)
+        {
+            try
+            {
+                typeOfDuck = Enum.GetName(typeof(typeOfDucks), duckNumber);
+            }
+            catch
+            {
+                Console.WriteLine("Wrong duck number");
+            }
+        }
+        public void setFlyStatus()
+        {
+            flyStatus = "Fly fast";
+        }
+        public void setQueakStatus()
+        {
+            quackStatus = "Quack loud";
+        }
+    }
+    public class Redhead :Duck, IDuckProperties
+    {
+        public void setWeight(int weight)
+        {
+            this.weight = weight;
+        }
+        public void setNumberOfWings(int numberOfWings)
+        {
+            this.numberOfWings = numberOfWings;
+        }
+        public void setTypeOfDuck(int duckNumber)
+        {
+            try
+            {
+                typeOfDuck = Enum.GetName(typeof(typeOfDucks), duckNumber);
+            }
+            catch
+            {
+                Console.WriteLine("Wrong duck number");
+            }
+        }
+        public void setFlyStatus()
+        {
+            flyStatus = "Fly slow";
+        }
+        public void setQueakStatus()
+        {
+            quackStatus = "Quack mild";
+        }
+    }
+}
+
